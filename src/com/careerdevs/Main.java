8818f98a559cdf3e1d1847357e5ba113c2006812
marcusj12 <marcus.a.jennings12@gmail.com>
2022-02-22 19:57:52 -0500

    Word Guesser Game

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,19 @@
 package com.careerdevs;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       // mainMenu();
+    }
+
+    private static void mainMenu () {
+        System.out.println("-".repeat(3) + "Main Menu" + "-".repeat(3));
+
+        System.out.println("1) Play Game");
+        System.out.println("2) Exit");
+
+
     }
 }
