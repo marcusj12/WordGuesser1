7c366c5c8ca35ae0d15738cb5afbae95471046ba
marcusj12 <marcus.a.jennings12@gmail.com>
2022-03-01 17:45:12 -0500

    Java Algorithms ; Edabit Challenges

---
+++ src/com/careerdevs/EdabitChallenges/Challenges.java
@@ -0,0 +1,40 @@
+package com.careerdevs.EdabitChallenges;
+
+public class Challenges {
+    public static void main(String[] args) {
+        System.out.println(charCount('l',"Hello"));
+        System.out.println(getCount("beautiful"));
+        System.out.println(getVowels("American Dad"));
+
+    }
+
+    public static int charCount(char c, String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i)== c)
+                count++;
+        }
+        return count;
+    }
+
+    public static int getCount(String str) {
+            int vowelsCount = 0;
+            for (int i = 0; i < str.length(); i++) {
+
+            }
+
+            return vowelsCount;
+    }
+
+        public static int getVowels(String str) {
+            int vowelsCount = 0;
+            String word = word.toLowerCase();
+                for (int i = 0; i < word.length(); i++) {
+                    if (char == "a" || char || "e")
+
+
+                }
+
+                    return vowelsCount;
+        }
+    }
